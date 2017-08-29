8c019b13c6dd8df60288e477f877e96e6965d252
Ray <li40qt@hotmail.com>
2017-08-28 22:12:15 -0400

    Bubble Sort

--- BubbleSortOptimized.java
+++ BubbleSortOptimized.java
@@ -7,25 +7,27 @@ public class BubbleSortOptimized {
 
 		//Initializing array of random numbers
 		
-		int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+		int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-		//int[] array = new int[20];
+		//int[] array = new int[10];
 		//Generating random numbers
-		Random rand = new Random();
-
-		for(int index = 0; index < array.length; index++){
-			//Maximum 50  Minimum 1
-			array[index] = rand.nextInt(50) + 1;
-		}
+//		Random rand = new Random();
+//
+//		for(int index = 0; index < array.length; index++){
+//			//Maximum 50  Minimum 1
+//			array[index] = rand.nextInt(50) + 1;
+//		}
 
 		//Display array
 		for(int n: array){
 			System.out.print(n + " ");
 		}
 		System.out.println();
+		System.out.println();
 
 		//Bubble Sort;
 		//Time complexcity §°(n^2)
+		//Best Case: O(n)
 		boolean isSwap = true;
 		int rounds = 0;
 		while(isSwap == true){
@@ -33,7 +35,6 @@ public class BubbleSortOptimized {
 			isSwap = false;
 
 			for(int index = 1; index < array.length - rounds; index++){
-				
 				System.out.println(index);
 				
 				if(array[index-1] > array[index]){
@@ -41,7 +42,9 @@ public class BubbleSortOptimized {
 					isSwap = true;
 				}
 			}
-
+			
+			System.out.println();
+			
 			rounds++;
 
 		}
@@ -52,6 +55,7 @@ public class BubbleSortOptimized {
 			System.out.print(n + " ");
 		}
 		System.out.println();
+		
 	}
 
 	public static void swap(int[] array, int index1, int index2){
